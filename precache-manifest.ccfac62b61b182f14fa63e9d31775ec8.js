40042b09446054ab72e18e34aea1a1b126db306a
manuelnelson <elnels@gmail.com>
2019-03-16 12:41:05 -0400

    deploy

---
+++ precache-manifest.ccfac62b61b182f14fa63e9d31775ec8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "eeca88a269d1439604eb",
+    "url": "/js/chunk-vendors.7613e50f.js"
+  },
+  {
+    "revision": "6fb77154ae755ef73a26",
+    "url": "/js/app.9a8544a7.js"
+  },
+  {
+    "revision": "26fee0059ec098d5f609",
+    "url": "/js/about.d288b4f1.js"
+  },
+  {
+    "revision": "787395d2607574ae9980aa27aead5498",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fb77154ae755ef73a26",
+    "url": "/css/app.ac906db3.css"
+  }
+];
